9ec0dea60c9a17f652602cbf03470403898d0833
Mila <Ruzick@users.noreply.github.com>
2021-08-22 12:32:03 -0400

    added new functions to accomodate the new models

--- katseg/inference.py
+++ katseg/inference.py
@@ -7,6 +7,175 @@ from PIL import Image, ImageOps
 from matplotlib import gridspec
 
 
+class DeepLabModel(object):
+  """Class to load deeplab model and run inference."""
+
+  INPUT_TENSOR_NAME = 'ImageTensor:0'
+  OUTPUT_TENSOR_NAME = 'SemanticPredictions:0'
+  INPUT_SIZE = 513
+  FROZEN_GRAPH_NAME = 'frozen_inference_graphDLv3_mnv2_dm05_pascal'
+
+  def __init__(self,model, tarball_path ):
+    """Creates and loads pretrained deeplab model."""
+    self.graph = tf.Graph()
+    self.model = model
+    
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(ROOT_DIR + '/static/model/')
+    with tf.io.gfile.GFile(path+f'{model}', "rb") as f:
+      graph_def = tf.compat.v1.GraphDef()
+      graph_def.ParseFromString(f.read())
+
+    if graph_def is None:
+      raise RuntimeError('Cannot find inference graph in tar archive.')
+
+    with self.graph.as_default():
+      tf.import_graph_def(graph_def, name='')
+
+
+
+    self.sess = tf.compat.v1.Session(graph=self.graph)
+
+  def run(self, image):
+        """Runs inference on a single image.
+
+        Args:
+        image: A PIL.Image object, raw input image.
+
+        Returns:
+        resized_image: RGB image resized from original input image.
+        seg_map: Segmentation map of `resized_image`.
+        """
+        width, height = image.size
+        resize_ratio = 1.0 * self.INPUT_SIZE / max(width, height)
+        target_size = (int(resize_ratio * width), int(resize_ratio * height))
+        resized_image = image.convert('RGB').resize(target_size, Image.ANTIALIAS)
+        batch_seg_map = self.sess.run(
+            self.OUTPUT_TENSOR_NAME,
+            feed_dict={self.INPUT_TENSOR_NAME: [np.asarray(resized_image)]})
+        seg_map = batch_seg_map[0]
+        return resized_image, seg_map
+
+
+def get_category2(img,model):
+    """Write a Function to Predict the Class Name
+    Args:
+        img [jpg]: image file
+        model[tflite]
+    Returns:
+        [str]: Prediction
+    """
+    #Prepare iage further for running inference *******
+    # ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+    # path = os.path.join(ROOT_DIR + '/static/model/')
+
+ 
+    # with open(path + tflite_model_file, 'rb') as fid:
+    #     tflite_model = fid.read()
+
+    #read image and make it into pillow object
+    input_img = read_image(img)
+    image = format_image(input_img)
+
+    def create_pascal_label_colormap():
+        """Creates a label colormap used in PASCAL VOC segmentation benchmark.
+
+        Returns:
+            A Colormap for visualizing segmentation results.
+        """
+        colormap = np.zeros((256, 3), dtype=int)
+        ind = np.arange(256, dtype=int)
+
+        for shift in reversed(range(8)):
+            for channel in range(3):
+                colormap[:, channel] |= ((ind >> channel) & 1) << shift
+            ind >>= 3
+
+        return colormap
+
+
+    def label_to_color_image(label):
+        """Adds color defined by the dataset colormap to the label.
+
+        Args:
+            label: A 2D array with integer type, storing the segmentation label.
+
+        Returns:
+            result: A 2D array with floating type. The element of the array
+            is the color indexed by the corresponding element in the input label
+            to the PASCAL color map.
+
+        Raises:
+            ValueError: If label is not of rank 2 or its value is larger than color
+            map maximum entry.
+        """
+        if label.ndim != 2:
+            raise ValueError('Expect 2-D input label')
+
+        colormap = create_pascal_label_colormap()
+
+        if np.max(label) >= len(colormap):
+            raise ValueError('label value too large.')
+
+        return colormap[label]
+
+
+    def vis_segmentation(image, seg_map, model):
+        """Visualizes input image, segmentation map and overlay view."""
+        plt.figure(figsize=(15, 5))
+        grid_spec = gridspec.GridSpec(1, 4, width_ratios=[6, 6, 6, 1])
+
+        plt.subplot(grid_spec[0])
+        plt.imshow(image)
+        plt.axis('off')
+        plt.title('input image')
+
+        plt.subplot(grid_spec[1])
+        seg_image = label_to_color_image(seg_map).astype(np.uint8)
+        plt.imshow(seg_image)
+        plt.axis('off')
+        plt.title('segmentation map')
+
+        plt.subplot(grid_spec[2])
+        plt.imshow(image)
+        plt.imshow(seg_image, alpha=0.7)
+        plt.axis('off')
+        plt.title('segmentation overlay')
+
+        unique_labels = np.unique(seg_map)
+        ax = plt.subplot(grid_spec[3])
+        plt.imshow(
+            FULL_COLOR_MAP[unique_labels].astype(np.uint8), interpolation='nearest')
+        ax.yaxis.tick_right()
+        plt.yticks(range(len(unique_labels)), LABEL_NAMES[unique_labels])
+        plt.xticks([], [])
+        ax.tick_params(width=0.0)
+        plt.grid('off')
+        # plt.show()
+        ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+        path_pic = os.path.join(ROOT_DIR + '/static/images/')
+        fig = plt.gcf()
+        # fig.savefig(f'static/images/pic_{model}.png')
+        fig.savefig(path_pic+f'pic_{model}.png')
+
+
+    LABEL_NAMES = np.asarray([
+        'background', 'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus',
+        'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike',
+        'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tv'
+    ])
+
+    FULL_LABEL_MAP = np.arange(len(LABEL_NAMES)).reshape(len(LABEL_NAMES), 1)
+    FULL_COLOR_MAP = label_to_color_image(FULL_LABEL_MAP)
+
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(ROOT_DIR + '/static/model/')
+
+    MODEL = DeepLabModel(model,path+model)
+    resized_im, seg_map = MODEL.run(image)
+    im_output = vis_segmentation(resized_im, seg_map, model=model) 
+
+    return im_output
 def read_image(image):
     """
     reads image and transfomrs it into numpy array 
--- katseg/routes.py
+++ katseg/routes.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
 from flask import current_app as app
-from .inference import get_category,save_image
+from .inference import get_category,save_image,get_category2
 from werkzeug.utils import secure_filename
 import os
 # app = Flask(__name__)
@@ -26,13 +26,17 @@ def fragment():
         model1 = 'modelDeepLabV3_Mila.tflite'
         # model2 = 'lite-model_deeplabv3-xception65_1_default_2.tflite'
         model3 = 'lite-model_mobilenetv2-coco_dr_1.tflite'
+        model4 = 'frozen_inference_graphDLv3_mnv2_dm05_pascal.pb'
+        model5= 'frozen_inference_graphDLV3_mnv2_pascal_trainaug.pb'
         get_category(img=file, model =model1 )
         # get_category(img=file, model =model2 )
         get_category(img=file, model =model3 )
+        get_category2(img=file, model =model4)
+        get_category2(img=file, model =model5 )
         # category2 = get_category(img=file, model ='lite-model_mobilenetv2-coco_dr_1.tflite')
         # category3 = get_category(img=file, model='lite-model_deeplabv3-xception65_1_default_2.tflite')
         # Render the result template
-        return render_template('result.html', model1=model1, model3=model3)
+        return render_template('result.html', model1=model1, model3=model3,model4=model4,model5=model5)
         # from flask import Response
         # return Response( category1.getvalue(), mimetype='image/png')  # mimetype='image/png',  render_template('result.html', category=category, current_time=current_time)
         # # return Response(response = render_template('result.html'), category1.getvalue(), mimetype='image/png') # render_template('result.html', category=category, current_time=current_time)
